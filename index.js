fe7f128b21f665ac5067bd81d34cdebab9d0b7b7
FireInIce <miaoxingren2006@163.com>
2022-04-10 10:28:54 +0800

    update

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ function start(){
 	world=new cp.World((ctx,canvas)=>{
 		ctx.clearRect(0,0,canvas.width,canvas.height);
 	});
-	const size=7;
+	const size=10;
 	var emitter=new cp.PointCircleEmitter(canvas.width/2,canvas.height/2,100,new cp.BallPaintFunc(size,null,true));
 	// world.addForce(cp.forces.getMouseAwayForce(canvas,10000,100));
-	for(var i=0;i<1500;i++){
+	for(var i=0;i<900;i++){
 		var particle=emitter.emit();
 		world.addParticle(particle);
 		(function(){
@@ -32,12 +32,13 @@ function start(){
 			obj.alpha-=t/2;
 			obj.scale-=t;
 			return true;
-		},1);
+		},0.6);
 	}
 	var pos_gener=new cp.PosGenerator();
-	var poses=[],str="祝姥姥福如东海长流水寿比南山不老松";
+	var poses=[],str=["祝","姥姥","福","如","东","海","长","流","水","寿","比","南","山","不","老","松"];
 	for(var i=0;i<str.length;i++){
-		poses.push(pos_gener.getPosFromText(str[i],'50px 微软雅黑',true,10,(canvas.width-500)/2,(canvas.height-500)/2));//todo: set x,y
+		var len=str[i].length;
+		poses.push(pos_gener.getPosFromText(str[i],'30px 微软雅黑',true,15,(canvas.width-30*15*len)/2,(canvas.height-30*15)/2));//todo: set x,y
 	}
 	var dfunc=cp.distributes.getLineDistribute(cp.easeFunctions['easeInOutBack']);
 	// var dfunc=cp.distributes.getCircleDistribute(cp.easeFunctions['easeInOutBack']);
@@ -62,8 +63,9 @@ function start(){
 	var nfunc=(pos,time)=>{};
 	var i=0;
 	setInterval(()=>{
-		world.distributePosByDistance(poses[i],2,tdfunc,spfunc,nfunc,6);
+		world.distributePosByDistance(poses[i],2,tdfunc,spfunc,nfunc,size);
 		i=(i+1)%poses.length;
+		console.log(i);
 	},4000);
 	document.getElementById('audio').play();
 }
